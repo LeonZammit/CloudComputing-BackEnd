dd7a1dbac182c7bf0f106cb7ec0863db01975939
LeonZammit <80625286+LeonZammit@users.noreply.github.com>
2023-04-11 16:47:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import random 
+import random
+import os 
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+
+#Configure database connection
+db_user = "number-handler"
+db_pass = "fxcQ25c84dFhWTrfoBzUdlyj"
+db_name = "storage-number"
+cloud_sql_instance_name = "cloudassignment-383409:europe-west1:db-instance"
+
+def ConnectToDatabase():
+    config = {
+        'user': db_user,
+        'password': db_pass,
+        'database': db_name,
+        'unix_socket': f'/cloudsql/{cloud_sql_instance_name}'
+    }
+
+    return mysql.connector.connect(**config)
 
 @app.route('/GenerateRandomNumber')
 def GenerateRandomNumber():
